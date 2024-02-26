f1b1bd958b16ba7f5895e52a2d12235cb2cc9c05
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-26 09:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+  // DOM is loaded, let's write the string
+  var body = document.body;
+  body.innerHTML = 'DOM load success';
+});
